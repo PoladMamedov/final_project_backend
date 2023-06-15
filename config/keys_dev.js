456ffb05d2d8b13f0c7dde4407f875aeb5779122
Polad Mamedov <poladikksp@gmail.com>
2023-06-15 11:25:28 +0300

    configured the server

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    'Сюда копируем URI для доступа в БД, например: mongodb+srv://username:password@..........................',
-  secretOrKey: 'random very very secret string',
+    "mongodb+srv://poladmamedov:I1lf1B4MTWjtJkEl@finalprojectdb.llvrm8v.mongodb.net/?retryWrites=true&w=majority",
+  secretOrKey: "f56cd48db9b9be1a676fbad9aa1ac74e91b7077a53dadf45f2d70a7034957237",
 };
