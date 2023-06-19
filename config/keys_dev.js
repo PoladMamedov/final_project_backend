cb6529d49bb3cfb5bd6c77e2fb21fa238d8e3e75
Polad Mamedov <poladikksp@gmail.com>
2023-06-19 19:19:28 +0300

    angelikas

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://poladmamedov:I1lf1B4MTWjtJkEl@finalprojectdb.llvrm8v.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://oasisinnovat:admin@cluster0.9grqccg.mongodb.net/api",
   secretOrKey: "f56cd48db9b9be1a676fbad9aa1ac74e91b7077a53dadf45f2d70a7034957237",
 };
